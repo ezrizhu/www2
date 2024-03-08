4c49bfbce68200bfce56803a4de0bcf2fad79382
Ezri Zhu <me@ezrizhu.com>
2024-03-08 00:45:47 -0500

    did update

--- src/atproto.rs
+++ src/atproto.rs
@@ -4,6 +4,6 @@ pub async fn did() -> (HeaderMap, String) {
     let mut resp_header = HeaderMap::new();
     resp_header.insert("Content-Type", "text/plain".parse().unwrap());
     resp_header.insert("Access-Control-Allow-Origin", "*".parse().unwrap());
-    let did = String::from("did:plc:excwqwknn6u3eel2frn42eqm");
+    let did = String::from("did:plc:is4tunpj7uynbttcsxz66lu2");
     (resp_header, did)
 }
