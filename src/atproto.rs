a673e92fa6717503d78fff1d9e7b8f2e7d93525f
Ezri Zhu <me@ezrizhu.com>
2024-10-22 15:06:58 -0400

    fix bsky

--- src/atproto.rs
+++ src/atproto.rs
@@ -4,6 +4,6 @@ pub async fn did() -> (HeaderMap, String) {
     let mut resp_header = HeaderMap::new();
     resp_header.insert("Content-Type", "text/plain".parse().unwrap());
     resp_header.insert("Access-Control-Allow-Origin", "*".parse().unwrap());
-    let did = String::from("did:plc:is4tunpj7uynbttcsxz66lu2");
+    let did = String::from("did:plc:2tjbv3ubqhvurmrd337n567s")
     (resp_header, did)
 }
